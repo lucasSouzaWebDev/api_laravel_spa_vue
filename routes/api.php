2ce8ee8909a47640f57c13c05f241a9e2ba912ae
lucasSouzaWebDev <lucasdesouzadubena@gmail.com>
2021-12-14 19:51:55 -0300

    Upload de imagem de perfil

--- routes/api.php
+++ routes/api.php
@@ -93,10 +93,65 @@ Route::middleware('auth:api')->put('/perfil', function (Request $request) {
     }
     $user->name = $data['name'];
     $user->email = $data['email'];
+    if(isset($data['imagem'])){
+
+        Validator::extend('base64image', function ($attribute, $value, $parameters, $validator) {
+            $explode = explode(',', $value);
+            $allow = ['png', 'jpg', 'svg','jpeg'];
+            $format = str_replace(
+                [
+                    'data:image/',
+                    ';',
+                    'base64',
+                ],
+                [
+                    '', '', '',
+                ],
+                $explode[0]
+            );
+            // check file format
+            if (!in_array($format, $allow)) {
+                return false;
+            }
+            // check base64 format
+            if (!preg_match('%^[a-zA-Z0-9/+]*={0,2}$%', $explode[1])) {
+                return false;
+            }
+            return true;
+        });
+
+        $validacao = Validator::make($data, [
+            'imagem' => 'base64image',
+        ], array('base64image' => 'Imagem inválida!'));
+
+
+        $time = time();
+        $directory = 'perfis';
+        $imageDirectory = $directory . DIRECTORY_SEPARATOR . 'perfil_id'.$user->id;
+        $ext = substr($data['imagem'], 11, strpos($data['imagem'], ';') -11);
+        $urlImagem = $imageDirectory . DIRECTORY_SEPARATOR .$time.'.'.$ext;
+
+        $file = str_replace('data:image/'.$ext.';base64,', '', $data['imagem']);
+        $file = base64_decode($file);
+
+        if(!file_exists($directory)){
+            mkdir($directory, 0700);
+        }
+        if($user->imagem){
+            if(file_exists($user->imagem)){
+                unlink($user->imagem);
+            }
+        }
+        if(!file_exists($imageDirectory)){
+            mkdir($imageDirectory, 0700);
+        }
+        file_put_contents($urlImagem, $file);
+
+        $user->imagem = $urlImagem;
+    }
     $user->save();
 
-    
-
+    $user->imagem = asset($user->imagem);
     $user->token = $user->createToken($user->email)->accessToken;
     return $user;
 });
