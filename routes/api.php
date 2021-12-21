2c876df9102f369d51b51b38d6d785cde8638b76
lucasSouzaWebDev <lucasdesouzadubena@gmail.com>
2021-12-21 11:13:14 -0300

    Concluido publicação de conteudo

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use App\Comentario;
 Route::post('/cadastro', "UsuarioController@cadastro");
 Route::post('/login', "UsuarioController@login");
 Route::middleware('auth:api')->put('/perfil', "UsuarioController@perfil");
+Route::middleware('auth:api')->post('/conteudo/adicionar', "ConteudoController@adicionar");
 
 Route::get('/testes', function (){
     $user = User::find(1);
