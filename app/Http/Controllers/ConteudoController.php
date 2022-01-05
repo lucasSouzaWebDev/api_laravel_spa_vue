13c258a3612b8e4a4fd91cdcf0304b28616d31b6
lucasSouzaWebDev <lucasdesouzadubena@gmail.com>
2022-01-05 11:14:56 -0300

    Pegando dados do dono da página

--- app/Http/Controllers/ConteudoController.php
+++ app/Http/Controllers/ConteudoController.php
@@ -52,7 +52,7 @@ class ConteudoController extends Controller
                 $conteudo->curtiu_conteudo = false;
             }
         }
-        return ['status' => true, 'conteudos' => $conteudos];
+        return ['status' => true, 'conteudos' => $conteudos, 'dono' => $donoDaPagina];
     }
 
     public function adicionar(Request $request)
